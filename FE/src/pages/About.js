f7107df793bdf043d8e0b08675d066e804948828
Chethiya <chethiya.20211136@iit.ac.lk>
2024-03-03 21:03:26 +0530

    initial commit

---
+++ FE/src/pages/About.js
@@ -0,0 +1,26 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <div className="bg-white bg-opacity-45 rounded-md p-8 my-24 text-center">
+      <h2 className="font-bold text-2xl my-4">About Us</h2>
+      <p>
+        Experience the ultimate in travel customization with our personalized
+        itinerary generator designed exclusively for tourists. Whether you're an
+        avid explorer seeking cultural gems, an adrenaline junkie craving
+        adventure, or a leisure traveler in pursuit of relaxation, our intuitive
+        tool tailors your journey to match your preferences. From iconic
+        landmarks and hidden gems to local hot spots, we curate a seamless
+        itinerary that captures the essence of your dream vacation. Say goodbye
+        to one-size-fits-all travel plans -- let us elevate your experience with
+        a bespoke itinerary that ensures every moment is uniquely yours
+      </p>
+
+      <button className="bg-slate-800 text-white font-medium rounded-full px-4 py-2 mt-4">
+        Read More
+      </button>
+    </div>
+  );
+};
+
+export default About;
