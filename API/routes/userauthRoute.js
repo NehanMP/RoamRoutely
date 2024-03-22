57abe33b4be54a2aeffa9ba197bf666c952d87c3
Nehan Perera <nehan.20220606@iit.ac.lk>
2024-03-22 21:20:18 +0530

    Delete API directory

--- API/routes/userauthRoute.js
+++
@@ -1,8 +0,0 @@
-import express from "express";
-import { signup, signin } from "../controllers/authController.js";
-const router = express.Router();
-
-router.post("/signUp", signup);
-router.post("/signIn", signin);
-export default router;
-// export as default so it can be imported however needed.
